38556fe365aaa54a5b9cd73ada7cebab447f880a
jeff <jeff@giftbit.com>
2018-01-22 10:14:05 -0800

    Dotnet: shopper token signing.

--- Lightrail.Test/src/ShopperTokenFactoryTest.cs
+++ Lightrail.Test/src/ShopperTokenFactoryTest.cs
@@ -1,5 +1,10 @@
+using Lightrail.Model;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
 using System;
+using System.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 
 namespace Lightrail.Test
@@ -8,8 +13,92 @@ namespace Lightrail.Test
     public class ShopperTokenFactoryTest
     {
         [TestMethod]
-        public void TestGenerateShopperToken()
+        public void TestGenerateShopperTokenForContactId()
         {
+            LightrailConfiguration.ApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnIjp7Imd1aSI6Imdvb2V5IiwiZ21pIjoiZ2VybWllIn19.XxOjDsluAw5_hdf5scrLk0UBn8VlhT-3zf5ZeIkEld8";
+            LightrailConfiguration.SharedSecret = "secret";
+
+            var shopperToken = Lightrail.ShopperTokenFactory.GenerateShopperToken(new ContactIdentifier { ContactId = "chauntaktEyeDee" }, 600);
+            Assert.IsNotNull(shopperToken);
+            Assert.AreEqual(shopperToken.Split(".").Length, 3);
+
+            JwtSecurityToken jwt = ReadToken(shopperToken);
+            Assert.AreEqual(jwt.Header.Alg, "HS256");
+            Assert.AreEqual(jwt.Header["ver"], (Int64)3);
+            Assert.AreEqual(jwt.Header["vav"], (Int64)1);
+            Assert.AreEqual(((JObject)jwt.Payload["g"])["gui"], "gooey");
+            Assert.AreEqual(((JObject)jwt.Payload["g"])["gmi"], "germie");
+            Assert.AreEqual(((JObject)jwt.Payload["g"])["coi"], "chauntaktEyeDee");
+            Assert.AreEqual(jwt.Payload.Iss, "MERCHANT");
+            Assert.IsNotNull(jwt.Payload.Iat);
+            Assert.IsNotNull(jwt.Payload.Exp);
+        }
+
+        [TestMethod]
+        public void TestGenerateShopperTokenForShopperId()
+        {
+            LightrailConfiguration.ApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnIjp7Imd1aSI6Imdvb2V5IiwiZ21pIjoiZ2VybWllIn19.XxOjDsluAw5_hdf5scrLk0UBn8VlhT-3zf5ZeIkEld8";
+            LightrailConfiguration.SharedSecret = "secret";
+
+            var shopperToken = Lightrail.ShopperTokenFactory.GenerateShopperToken(new ContactIdentifier { ShopperId = "zhopherId" }, 600);
+            Assert.IsNotNull(shopperToken);
+            Assert.AreEqual(shopperToken.Split(".").Length, 3);
+
+            JwtSecurityToken jwt = ReadToken(shopperToken);
+            Assert.AreEqual(jwt.Header.Alg, "HS256");
+            Assert.AreEqual(jwt.Header["ver"], (Int64)3);
+            Assert.AreEqual(jwt.Header["vav"], (Int64)1);
+            Assert.AreEqual(((JObject)jwt.Payload["g"])["gui"], "gooey");
+            Assert.AreEqual(((JObject)jwt.Payload["g"])["gmi"], "germie");
+            Assert.AreEqual(((JObject)jwt.Payload["g"])["shi"], "zhopherId");
+            Assert.AreEqual(jwt.Payload.Iss, "MERCHANT");
+            Assert.IsNotNull(jwt.Payload.Iat);
+            Assert.IsNotNull(jwt.Payload.Exp);
+        }
+
+        [TestMethod]
+        public void TestGenerateShopperTokenForUserSuppliedId()
+        {
+            LightrailConfiguration.ApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnIjp7Imd1aSI6Imdvb2V5IiwiZ21pIjoiZ2VybWllIn19.XxOjDsluAw5_hdf5scrLk0UBn8VlhT-3zf5ZeIkEld8";
+            LightrailConfiguration.SharedSecret = "secret";
+
+            var shopperToken = Lightrail.ShopperTokenFactory.GenerateShopperToken(new ContactIdentifier { UserSuppliedId = "luserSuppliedId" }, 600);
+            Assert.IsNotNull(shopperToken);
+            Assert.AreEqual(shopperToken.Split(".").Length, 3);
+
+            JwtSecurityToken jwt = ReadToken(shopperToken);
+            Assert.AreEqual(jwt.Header.Alg, "HS256");
+            Assert.AreEqual(jwt.Header["ver"], (Int64)3);
+            Assert.AreEqual(jwt.Header["vav"], (Int64)1);
+            Assert.AreEqual(((JObject)jwt.Payload["g"])["gui"], "gooey");
+            Assert.AreEqual(((JObject)jwt.Payload["g"])["gmi"], "germie");
+            Assert.AreEqual(((JObject)jwt.Payload["g"])["cui"], "luserSuppliedId");
+            Assert.AreEqual(jwt.Payload.Iss, "MERCHANT");
+            Assert.IsNotNull(jwt.Payload.Iat);
+            Assert.IsNotNull(jwt.Payload.Exp);
+        }
+
+        private JwtSecurityToken ReadToken(string shopperToken)
+        {
+            var keyBytes = Encoding.Default.GetBytes(LightrailConfiguration.SharedSecret);
+            if (keyBytes.Length < 64)
+            {
+                Array.Resize(ref keyBytes, 64);
+            }
+            var key = new SymmetricSecurityKey(keyBytes);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            SecurityToken token;
+            var handler = new JwtSecurityTokenHandler();
+            handler.ValidateToken(shopperToken,
+                        new TokenValidationParameters()
+                        {
+                            ValidateAudience = false,
+                            ValidateIssuer = false,
+                            IssuerSigningKey = key
+                        }, out token);
+
+            return (JwtSecurityToken) token;
         }
     }
 }
--- Lightrail/src/ShopperTokenFactory.cs
+++ Lightrail/src/ShopperTokenFactory.cs
@@ -1,4 +1,6 @@
+using Lightrail.Model;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens;
@@ -11,57 +13,73 @@ namespace Lightrail
     public static class ShopperTokenFactory
     {
         
-        public static string GenerateShopperToken(string shopperId, int validityInSeconds = 43200)
+        public static string GenerateShopperToken(ContactIdentifier contact, int validityInSeconds = 43200)
         {
             if (LightrailConfiguration.ApiKey == null)
             {
-                // TODO throw a better Exception type
-                throw new Exception("LightrailConfiguration.ApiKey is not set.");
+                throw new InvalidOperationException("LightrailConfiguration.ApiKey is not set.");
             }
             if (LightrailConfiguration.SharedSecret == null)
             {
-                // TODO throw a better Exception type
-                throw new Exception("LightrailConfiguration.SharedSecret is not set.");
+                throw new InvalidOperationException("LightrailConfiguration.SharedSecret is not set.");
             }
-            if (shopperId == null)
+            if (contact == null)
             {
-                throw new ArgumentNullException(nameof(shopperId));
+                throw new ArgumentNullException(nameof(contact));
+            }
+            if (contact.ContactId == null && contact.ShopperId == null && contact.UserSuppliedId == null)
+            {
+                throw new ArgumentException(nameof(contact), "one of ContactId, ShopperId or UserSupliedId must be set");
             }
             if (validityInSeconds < 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(validityInSeconds), "must be > 0");
             }
 
-            var key = new SymmetricSecurityKey(Encoding.Default.GetBytes(LightrailConfiguration.SharedSecret));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+            var handler = new JwtSecurityTokenHandler();
+            var merchantToken = (JwtSecurityToken) handler.ReadToken(LightrailConfiguration.ApiKey);
+            if (merchantToken.Payload["g"] == null || ((JObject)merchantToken.Payload["g"])["gui"] == null)
+            {
+                throw new InvalidOperationException("LightrailConfiguration.ApiKey is not valid.");
+            }
+
+            var keyBytes = Encoding.Default.GetBytes(LightrailConfiguration.SharedSecret);
+            if (keyBytes.Length < 64)
+            {
+                Array.Resize(ref keyBytes, 64);
+            }
+            var key = new SymmetricSecurityKey(keyBytes);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
             var header = new JwtHeader(creds)
             {
                 {"ver", 3},
                 {"vav", 1}
             };
 
+            var g = new Dictionary<string, object>
+            {
+                {"gui", ((JObject)merchantToken.Payload["g"])["gui"]},
+                {"gmi", ((JObject)merchantToken.Payload["g"])["gmi"]}
+            };
+            if (contact.ContactId != null) {
+                g["coi"] = contact.ContactId;
+            }
+            if (contact.ShopperId != null) {
+                g["shi"] = contact.ShopperId;
+            }
+            if (contact.UserSuppliedId != null) {
+                g["cui"] = contact.UserSuppliedId;
+            }
             var nowInSeconds = (int) (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
             var payload = new JwtPayload
             {
-                {
-                    "g",
-                    new Dictionary<string, object>
-                    {
-                        {"gui", "userid"},
-                        {"gmi", "merchantid"},
-                        {"coi", "contactid or undefined"},  // TODO decode LightrailConfiguration.ApiKey and use those values
-                        {"cui", "Contact usersuppliedid or undefined"},
-                        {"shi", "shopperid or undefined"}
-                    }
-                },
+                {"g", g},
                 {"iss", "MERCHANT"},
                 {"iat", nowInSeconds},
                 {"exp", nowInSeconds + validityInSeconds}
             };
 
             var secToken = new JwtSecurityToken(header, payload);
-
-            var handler = new JwtSecurityTokenHandler();
             return handler.WriteToken(secToken);
         }
     }
